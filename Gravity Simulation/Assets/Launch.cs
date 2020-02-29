27d55c467761ada789e28f09c0fa459a2207bd24
yuzhao <yuzhao5@illinois.edu>
2020-02-29 17:57:06 -0600

    Setup and Added Gravity

---
+++ Gravity Simulation/Assets/Launch.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class Launch : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject cylinder;
+
+    public Rigidbody asteroid;
+
+    private Vector3 direction;
+
+    void Start()
+    {
+        Launchat();
+        asteroid.AddForce(direction);
+    }
+
+    void Launchat()
+    {
+        direction = cylinder.transform.position;
+
+    }
+}
